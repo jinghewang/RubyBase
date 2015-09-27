03d8e0fa9bd27671e85ea36bbd644830e8becf8f
jinghewang <jinghewang@163.com>
2015-09-27 10:53:29 +0800

    times

--- 06/rec_test.rb
+++ 06/rec_test.rb
@@ -1,2 +1,18 @@
 # encoding:utf-8
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
+
+
+
+
+
+
+
+
+# times
+num = 5
+num.times do |i|
+  p i
+end
+
+num.times{|i| p "curren i:#{i}";}
+
