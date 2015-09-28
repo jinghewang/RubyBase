d16c1cb6d5eeb45b3d2d4a81326e123740f1dfe0
jinghewang <jinghewang@163.com>
2015-09-28 22:44:23 +0800

    scope test 3

--- 07/scope_test.rb
+++ 07/scope_test.rb
@@ -10,20 +10,40 @@ END{  puts '---------end------------'}
 
 puts '----------参数个数-----------------'
 
+=begin
 v1 = 1
 class MyClass
   v2 = 2
-  puts local_variables.to_s + "call 1"  # => [:v2]call 1
+  puts local_variables.to_s + " call 1"  # => [:v2]call 1
   def my_method
     v3 = 3
     puts local_variables.to_s + " call 2"
   end
-  puts local_variables.to_s + "call 3"  # => [:v2]call 3
+  puts local_variables.to_s + " call 3"  # => [:v2]call 3
 end
 
 obj = MyClass.new
 obj.my_method                           # =>[:v3] call 2
-puts local_variables.to_s + "call 4"    # =>[:v1, :obj]call 4
+puts local_variables.to_s + " call 4"    # =>[:v1, :obj]call 4
+=end
+
+
+# --------------------------------------
+puts '========================================'
+
+v1 = 1
+MyClass = Class.new do
+  v2 = 2
+  puts local_variables.to_s + "call 1"     # => [:v2, :v1]call 1
+  define_method :my_method do
+    v3 = 3
+    puts local_variables.to_s + " call 2"
+  end
+  puts local_variables.to_s + "call 3"      # => [:v2, :v1]call 3
+end
+MyClass.new.my_method                       # => [:v3, :v2, :v1] call 2
+puts local_variables.to_s + "call 4"        # => [:v1]call 4
+
 
 
 
