66fd13e662db2bf61e96c4e27f99d9d003a10a36
jinghewang <jinghewang@163.com>
2015-09-28 11:37:09 +0800

    block test update

--- 07/block_test.rb
+++ 07/block_test.rb
@@ -25,9 +25,46 @@ def mytimes
   end
 end
 
+def block_method(a, b)
+  return yield(a,b)
+end
+
+def check_block(a, b)
+  return yield(a,b) if block_given?
+  'no block'
+end
+
+def closure_method(y)
+  x = "GoodBye"
+  yield(y)
+end
+
 num = 1
 mytimes do
   puts "return num is:#{num}"
   num += 1
   break if num > 10
 end
+a, b = 3, 5
+
+
+
+
+# { } 方式
+p block_method(a, b) { |a, b| print "block_method a:#{a} b:#{b}\n"; a + b; }
+
+# do end 方式，不能直接输出，会报错，不知道为什么
+result = block_method(a, b) do |a, b|
+  print "block_method2 a:#{a} b:#{b}\n";
+  a + b;
+end
+p result
+
+# no block 测试
+puts check_block(a, b){|a, b| 'This is a block'}
+puts check_block(a, b)
+
+# 引用外部变量测试
+x = 'Hello'
+result = closure_method('@xianlinbox') { |y| print "#{x} World, #{y}" }
+p result
